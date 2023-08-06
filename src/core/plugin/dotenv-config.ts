46d901e1ddae600765b4a28efc7c4bec98c18681
Nanthakarn Limkool <nanthakarn18@gmail.com>
2023-08-06 18:02:16 +0700

    feat(core/plugin): update dotenv plugin

--- src/core/plugin/dotenv-config.ts
+++ src/core/plugin/dotenv-config.ts
@@ -1,10 +1,20 @@
-import { validate } from 'class-validator';
+import { IsIn, IsNotEmpty, IsPort, validate } from 'class-validator';
+import pino, { Level } from 'pino';
 import dotenv from 'dotenv';
 import fs from 'fs';
-// import Container from "typedi";
+import Container from 'typedi';
 
-export class EnvConfig {
-  // TODO: add config
+import { Logger } from './logger';
+
+export class DotEnvConfig {
+  @IsNotEmpty()
+  readonly CHATBOT_API_HOST: string = 'localhost';
+
+  @IsPort()
+  readonly CHATBOT_API_PORT: number = 8080;
+
+  @IsIn(Object.values(pino.levels.labels))
+  readonly LOG_LEVEL: Level = 'info';
 
   [x: string]: any;
 
@@ -15,22 +25,21 @@ export class EnvConfig {
   }
 }
 
-export function readConfigFromDotEnv(): EnvConfig {
-  // const logger: Logger = Container.get(LoggerContainerKey);
+export function readConfigFromDotEnv(): DotEnvConfig {
+  const logger: Logger = Container.get('Logger');
   const dotenvPath = fs.existsSync('.env') ? '.env' : 'common/config/.env.local';
-  // logger.info(`readConfigFromDotEnv .env path=${dotenvPath}`);
+  logger.info(`readConfigFromDotEnv .env path=${dotenvPath}`);
   dotenv.config({ path: dotenvPath });
-  const config = new EnvConfig();
+  const config = new DotEnvConfig();
 
   validate(config).then(errors => {
     if (errors.length > 0) {
-      // logger.error(`readConfigFromDotEnv validation failed. Errors: ${errors}`);
-      console.log(`readConfigFromDotEnv validation failed. Errors: ${errors}`);
+      logger.error(`readConfigFromDotEnv validation failed. Errors: ${errors}`);
       process.exit(1);
     }
-    // logger.info("readConfigFromDotEnv validation success.");
+    logger.info('readConfigFromDotEnv validation success.');
   });
 
-  // logger.debug(config);
+  logger.debug(config);
   return config;
 }
