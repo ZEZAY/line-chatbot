36ffe5cb9850ec938fb397ae82a4c20eaf773b67
Nanthakarn Limkool <nanthakarn18@gmail.com>
2023-08-08 13:02:44 +0700

    refactor: webhook-dto

--- src/domain/webhook/webhook-dto/delivery-context-dto.ts
+++
@@ -1,5 +0,0 @@
-import { Static, Type } from '@sinclair/typebox';
-
-export const DeliveryContextSchema = Type.Object({ isRedelivery: Type.Boolean() });
-
-export type DeliveryContext = Static<typeof DeliveryContextSchema>;
--- src/domain/webhook/webhook-dto/webhook-request-dto.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyRequest } from 'fastify';
-
-import { WebhookEvent } from './webhook-event-dto';
-
-interface WebhookRequestInterface extends FastifyRequest {
-  events: WebhookEvent[];
-}
-
-export type WebhookRequest = FastifyRequest<{ Body: WebhookRequestInterface }>;
